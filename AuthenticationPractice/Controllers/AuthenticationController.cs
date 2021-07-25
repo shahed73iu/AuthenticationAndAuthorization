5a9ec0457231ea8a9204bc9777611c850c15cfd5
Md. Emam Uddin Shahed <48101787+shahed73iu@users.noreply.github.com>
2021-07-25 16:02:15 +0600

    startup changed. AuthenticationController Added and dependency injection created in this controller

---
+++ AuthenticationPractice/Controllers/AuthenticationController.cs
@@ -0,0 +1,27 @@
+﻿using AuthenticationPractice.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationPractice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly RoleManager<ApplicationUser> roleManager;
+        private readonly IConfiguration _configuration;
+        public AuthenticationController(UserManager<ApplicationUser> userManager, RoleManager<ApplicationUser> roleManager, IConfiguration configuration)
+        {
+            this.userManager = userManager; 
+            this.roleManager = roleManager;
+            _configuration = configuration;
+        }
+    }
+}
--- AuthenticationPractice/Startup.cs
+++ AuthenticationPractice/Startup.cs
@@ -1,4 +1,5 @@
 using AuthenticationPractice.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -8,9 +9,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace AuthenticationPractice
@@ -32,7 +35,29 @@ namespace AuthenticationPractice
             services.AddDbContext<ApplicationDbContext>(option =>
                 option.UseSqlServer(Configuration.GetConnectionString("connectionString")));
             // For Identity
-            //services.AddIdentity<ApplicationUser, IdentityRole>().
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                                .AddEntityFrameworkStores<ApplicationDbContext>()
+                                .AddDefaultTokenProviders();
+            // Adding Authentication
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            // Adding jwt Bearer
+            .AddJwtBearer(options => {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = Configuration["JWT:ValidAudience"],
+                    ValidIssuer = Configuration["JWT:ValidIssuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:SecretKey"]))
+                };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
