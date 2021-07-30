f853035e8a2a21ec5ac162330ea3bebcfbce22c9
Md. Emam Uddin Shahed <48101787+shahed73iu@users.noreply.github.com>
2021-07-30 18:10:16 +0600

    Authentication Controller Setup done at this stage

--- AuthenticationPractice/Controllers/AuthenticationController.cs
+++ AuthenticationPractice/Controllers/AuthenticationController.cs
@@ -23,5 +23,27 @@ namespace AuthenticationPractice.Controllers
             this.roleManager = roleManager;
             _configuration = configuration;
         }
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel model)
+        {
+            var userExist = await userManager.FindByNameAsync(model.UserName);
+            if (userExist != null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User Already Exist" });
+            }
+            ApplicationUser user = new ApplicationUser()
+            {
+                Email = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = model.UserName
+            };
+            var result = await userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation Failed! " });
+            }
+            return Ok(new Response {  Status = "Success", Message = "User Created Successfully"});
+        }
     }
 }
